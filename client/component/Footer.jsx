e9aacaf4b58bc3e2bd12f0b301f670fc44abc737
Mitchell Cash <mitchell@mitchellcash.com>
2021-02-18 11:32:30 +0000

    Replace large footer image with SVG

--- client/component/Footer.jsx
+++ client/component/Footer.jsx
@@ -25,7 +25,7 @@ class Footer extends Component {
     return (
       <div className="footer">
         <div className="footer__block">
-          <img className="footer__logo" src="/img/footerlogo.png" />
+          <img className="footer__logo" src="/img/footerlogo.svg" />
           <span className="footer__legal">
             <div>Copyright &copy; 2018 <a href="https://ionomy.com/">ION</a></div>
             <div>Site design&copy; 2018 <a href="https://bulwarkcrypto.com/">Bulwark Cryptocurrency</a></div>
