6627249932aee0c9bdeb9b3cd8296962ebe3307d
Mitchell Cash <mitchell@mitchellcash.com>
2021-02-23 14:01:52 +0000

    Update list of exchanges

--- client/component/Card/CardExchanges.jsx
+++ client/component/Card/CardExchanges.jsx
@@ -5,9 +5,10 @@ import Card from './Card';
 
 const CardExchanges = () => (
   <Card title="Exchanges">
-    <a href="https://ionomy.com/en/markets/btc-ion" target="_blank" rel="noopener">Ionomy</a><br/>
-    <a href="https://bittrex.com/Market/Index?MarketName=BTC-ION" target="_blank" rel="noopener">Bittrex</a><br/>
-    <a href="https://upbit.com/exchange?code=CRIX.UPBIT.BTC-ION" target="_blank" rel="noopener">Upbit</a><br/>
+    <a href="https://exchange.ionomy.com/en/markets/btc-ion" target="_blank" rel="noopener">ionomy</a><br/>
+    <a href="https://crex24.com/exchange/ION-BTC" target="_blank" rel="noopener">CREX24</a><br/>
+    <a href="https://safe.trade/trading/ionbtc" target="_blank" rel="noopener">SafeTrade</a><br/>
+    <a href="https://app.stex.com/en/trade/pair/BTC/ION" target="_blank" rel="noopener">STEX</a><br/>
   </Card>
 );
 
